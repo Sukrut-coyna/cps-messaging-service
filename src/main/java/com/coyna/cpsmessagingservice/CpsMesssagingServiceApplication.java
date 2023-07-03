f4f30083a6bd34b55455d65141ac3e4721959ed0
NikhilSethuram <136423844+Nikhil-coyna@users.noreply.github.com>
2023-07-03 11:53:21 +0530

    setup
    initial commit

--- src/main/java/com/coyna/cpsmessagingservice/CpsMesssagingServiceApplication.java
+++ src/main/java/com/coyna/cpsmessagingservice/CpsMesssagingServiceApplication.java
@@ -1,13 +1,13 @@
-package com.coyna.cpstransactionservice;
+package com.coyna.cpsmessagingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CpsTransactionServiceApplication {
+public class CpsMesssagingServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CpsTransactionServiceApplication.class, args);
+		SpringApplication.run(CpsMesssagingServiceApplication.class, args);
 	}
 
 }
