eb921d8c2ae66047ecbc0d496fac6292a5aa2ac0
Nikhil-coyna <136423844+Nikhil-coyna@users.noreply.github.com>
2023-07-03 11:46:20 +0530

    setup

---
+++ cps-transaction-service-main/src/main/java/com/coyna/cpstransactionservice/CpsTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coyna.cpstransactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpsTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpsTransactionServiceApplication.class, args);
+	}
+
+}
---
+++ cps-transaction-service-main/src/test/java/com/coyna/cpstransactionservice/CpsTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coyna.cpstransactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpsTransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
